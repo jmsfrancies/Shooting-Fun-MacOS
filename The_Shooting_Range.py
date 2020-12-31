62b552876b3c5e40eaf778db387549ba8a76fa7e
James Francies <jfrancies1@live.maryville.edu>
2020-12-31 11:18:32 -0600

    shortened my total amount of code and I am deciding between a full auto function or just semi automatic

--- The_Shooting_Range.py
+++ The_Shooting_Range.py
@@ -2,32 +2,28 @@
 # however your game must be started by calling the main function in this file.
 from graphics import *
 import sys
+import time
 
-def Ready(win,title,HowManyRounds,WhatCaliber):
-  total = 0
-  target_color = ["Blue","Green","Yellow","Orange","Red"]
-  for i in reversed(range(30,171,35)):
-        Target = Circle(Point(250,250),i)
+
+def Ready(win, title):
+    total = 0
+    target_color = ["Blue", "Green", "Yellow", "Orange", "Red"]
+    for i in reversed(range(30, 171, 35)):
+        Target = Circle(Point(250, 250), i)
         Target.setFill("{0}".format(target_color[total]))
         Target.draw(win)
         total = total + 1
-        
-  
 
-  if int(HowManyRounds) >= 1:
-    for i in range(HowManyRounds):
-        if WhatCaliber == 1:    
-            BulletHole = win.getMouse()
-            x = BulletHole.getX()
-            y = BulletHole.getY()
-            BulletHole = Circle(Point(x,y),2.5)
-            BulletHole.setFill("black")
-            BulletHole.setOutline("white")
-            BulletHole.draw(win)
+    for i in range(25):
+        BulletHole = win.getMouse()
+        x = BulletHole.getX()
+        y = BulletHole.getY()
+        BulletHole = Circle(Point(x, y), 2.5)
+        BulletHole.setFill("black")
+        BulletHole.setOutline("white")
+        BulletHole.draw(win)
 
 
-
-      
     title.setText("Great Shooting! Please Come Again!")
     win.getMouse()
     win.getMouse()
@@ -35,9 +31,9 @@ def Ready(win,title,HowManyRounds,WhatCaliber):
 
 
 def main():
-    win = GraphWin("The Shooting Range",500,500)
+    win = GraphWin("The Shooting Range", 500, 500)
     win.setBackground("white")
-    title = Text(Point(250,25),"""Welcome to The Machine Gun Shooting Range!
+    title = Text(Point(250, 25), """Welcome to The Machine Gun Shooting Range!
 Where Precision and accuracy get further refined!""")
     title.setSize(20)
     title.setStyle("bold italic")
@@ -45,6 +41,7 @@ Where Precision and accuracy get further refined!""")
     HowManyRounds = 30
     WhatCaliber = 1
     win.getMouse()
-    Ready(win,title,HowManyRounds,WhatCaliber)
- 
+    Ready(win, title)
+
+
 main()
